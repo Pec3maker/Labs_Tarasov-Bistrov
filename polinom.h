17538c25ffe3a9aecca10c3174b9533c56dd4e73
Artem <38285312+Pec3maker@users.noreply.github.com>
2020-09-12 11:03:55 +0300

    Create polinom.h

---
+++ polinom.h
@@ -0,0 +1,36 @@
+#pragma once
+//polinom.h
+#include <iostream>
+
+//Спецификация класса
+class Polinom
+{
+public:
+	Polinom() = default;
+	Polinom(int degree, double* multipliers);
+	Polinom(const Polinom& source);
+
+	//Деструктор
+	~Polinom();
+
+	double SolvePoli(double arg) const; //Вычисление значения полинома
+	void SetPoli(int degree, double* multipliers); //Ввод полинома
+	Polinom AddPoli(Polinom& source); //Прибавить полином
+	Polinom SubstractPoli(Polinom& source); //Вычесть полином
+	int GetDegree() const;
+
+
+
+	//Перегрузки
+	Polinom operator = (const Polinom&);
+	Polinom operator + (const Polinom&) const;
+	Polinom operator - (Polinom&) const;
+	Polinom operator - () const;
+	double operator [] (const int index) const;
+	friend std::ostream& operator << (std::ostream&, const Polinom&);
+
+private:
+	int _degree = 0;
+	double* _multipliers = nullptr;
+	void cpy(Polinom& target, const int degree, const double* multiplier);
+};
