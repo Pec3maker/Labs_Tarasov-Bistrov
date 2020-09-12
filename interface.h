a3c1c2b6edd12b2fb533c9c598467f847edbbec7
Artem <38285312+Pec3maker@users.noreply.github.com>
2020-09-12 13:59:59 +0300

    Update interface.h

--- interface.h
+++ interface.h
@@ -1,6 +1,6 @@
-//interface.h
 #pragma once
 #include "polinom.h"
+//interface.h
 
 class Interface 
 {
@@ -8,15 +8,15 @@ public:
 	Interface();
 	~Interface();
 
-	void Init();
+	void Init() const;
 private:
-	void CreatePoli();
-	void DeletePoli(std::string name);
-	void Help();
-	void Minus();
-	void Plus();
-	int GetIndex(std::string name);
-	void EditPoli(std::string name);
+	void CreatePoli() const;
+	void DeletePoli(std::string const name) const;
+	void Help() const;
+	void Minus() const;
+	void Plus() const;
+	int GetIndex(std::string const name) const;
+	void EditPoli(std::string const name) const;
 
 	std::string* _names = nullptr;
 	Polinom* _cases = nullptr;
