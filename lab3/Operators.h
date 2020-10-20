8c02a4258ccfa3ad00b6b2f0a25f498eb51835a2
Artem <38285312+Pec3maker@users.noreply.github.com>
2020-10-20 17:48:34 +0300

    Update Operators.h

--- lab3/Operators.h
+++ lab3/Operators.h
@@ -3,10 +3,10 @@
 class Operators
 {
 public:
-	int Compare(Shape* shape1, Shape* shape2) const;
-	bool IsIntersect(Shape* shape1, Shape* shape2) const;
-	bool IsInclude(Shape* shape1, Shape* shape2) const;
+	int Compare(Shape& shape1, Shape& shape2) const;
+	bool IsIntersect(Shape& shape1, Shape& shape2) const;
+	bool IsInclude(Shape& shape1, Shape& shape2) const;
 
 	double GetLength(Point& point1, Point& point2) const;
-	double GetMaxRadius(Shape* shape) const;
+	double GetMaxRadius(Shape& shape) const;
 };
